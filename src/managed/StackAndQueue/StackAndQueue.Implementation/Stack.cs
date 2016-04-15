7d407683ba2dc0460131ac27544a8d9d3a8b5905
Gabrijel Boduljak <gboduljak@outlook.com>
2016-04-15 21:36:49 +0200

    started implementing stack

--- src/managed/StackAndQueue/StackAndQueue.Implementation/Stack.cs
+++ src/managed/StackAndQueue/StackAndQueue.Implementation/Stack.cs
@@ -1,6 +1,13 @@
 ﻿namespace StackAndQueue.Implementation
 {
-    public class Stack
+    public class StackElement<T>
     {
+        public T                 Value  { get; set; }
+        public StackElement<T>   Next   { get; set; }
+    }
+    public class Stack<T>
+    {
+        public StackElement<T>   Head   { get; set; }
+        public int               Count  { get; set; } = 0;
     }
 }
