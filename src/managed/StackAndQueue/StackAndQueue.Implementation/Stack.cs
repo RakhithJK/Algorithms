811988571aa622cf08de9ca8bb8a87c1430e71f7
Gabrijel Boduljak <gboduljak@outlook.com>
2016-04-15 23:53:55 +0200

    implemented stack with linked list approach

--- src/managed/StackAndQueue/StackAndQueue.Implementation/Stack.cs
+++ src/managed/StackAndQueue/StackAndQueue.Implementation/Stack.cs
@@ -9,5 +9,37 @@
     {
         public StackElement<T>   Head   { get; set; }
         public int               Count  { get; set; } = 0;
+
+        public void Push(T value)
+        {
+            Push(new StackElement<T> { Value = value });
+        }
+           
+        public void Push (StackElement<T> element)
+        {
+            if (Head == null)
+            {
+                Head = element;
+                Count++;
+            }
+            else
+            {
+                var nextElement = Head;
+                Head = element;
+                Head.Next = nextElement;
+                Count++;
+            }
+        }
+        public T Pop()
+        {
+            var nodeToReturn = Head;
+            Head = Head.Next;
+            Count--;
+            return nodeToReturn.Value;
+        }
+        public T Peek()
+        {
+            return (Count > 0) ? Head.Value : default(T);
+        }
     }
 }
