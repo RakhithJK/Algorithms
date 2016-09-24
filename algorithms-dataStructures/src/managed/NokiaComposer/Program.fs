919dfc3b1dc68a72a8e1c5c39a0100faed228001
Gabrijel Boduljak <gabrijel@dump.hr>
2016-09-24 12:58:57 +0200

    wrote simple nokia composer sound format parser

---
+++ algorithms-dataStructures/src/managed/NokiaComposer/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+open FParsec
+open composition_parser
+
+[<EntryPoint>]
+let main argv = 
+    let parseCompositionFromStream =
+     parse_composition  >>
+     (fun result -> 
+        match result with
+        | Success(result, _, _) -> sprintf "%A" result |> ignore
+        | Failure(error, _, _)  -> sprintf "Error"     |> ignore
+      )
+    let composition = "32f2 32#d2 32f2 8#d2 32#d2 32#d2 32f2 32g2 32f2 16.#d2 32- 16f2 8#d2 16#g2 32#g2 32#g2 32#g2 16g2 16.#d2 32- 8#a2 32#a2 32#a2 16#a2 16f2 16g2 8#g2 16g2 16g2 32g2 16g2 16d2 16#d2 8f2 16f2 8#d2 16#g2 32#g2 32#g2 32#g2 32g2 32#d2 32f2 16#d2"
+    parseCompositionFromStream composition 
+
+    Console.ReadKey()
+    0 // return an integer exit code
+//
---
+++ algorithms-dataStructures/src/managed/NokiaComposer/composition_parser.fs
@@ -0,0 +1,62 @@
+﻿module composition_parser
+open FParsec
+
+type measureFraction = Half | Quarter | Eight | Sixteen | Thirtysecond
+type note            = A | ASharp | B | C | CSharp | D | DSharp | E | F | FSharp | G | GSharp
+type octave          = One | Two | Three
+type sound           = Rest | Tone of note : note * octave : octave
+
+type length          = { fraction : measureFraction; isExtended : bool }
+type token           = { length : length; sound : sound }
+
+let parse_measureFraction = 
+    (stringReturn "2"  Half)            <|> 
+    (stringReturn "4"  Quarter)         <|>
+    (stringReturn "8"  Eight)           <|>
+    (stringReturn "16" Sixteen)         <|>
+    (stringReturn "32" Thirtysecond)
+let parse_extended_measureFraction = (stringReturn "." true) <|> (stringReturn "" false)
+let parse_length = 
+    pipe2 
+        parse_measureFraction
+        parse_extended_measureFraction
+        (fun parsedFraction parsedIsExtended -> { fraction = parsedFraction; isExtended = parsedIsExtended })
+         
+let parse_nonSharpNote = 
+    anyOf "be" |>> (function 
+    | 'b' -> B
+    | 'e' -> E
+    | unknown -> failwith <| string unknown)
+let parse_sharp = (stringReturn "#" true) <|> (stringReturn "" false)
+let parse_sharpNote =
+    pipe2
+        parse_sharp
+        (anyOf "acdefg")
+        (fun isNoteSharp note ->
+            match (isNoteSharp, note) with 
+            | (false, 'a') -> A      
+            | (true,  'a') -> ASharp 
+            | (false, 'c') -> C      
+            | (true,  'c') -> CSharp 
+            | (false, 'd') -> D      
+            | (true,  'd') -> DSharp 
+            | (false, 'f') -> F    
+            | (true,  'f') -> FSharp 
+            | (false, 'g') -> G   
+            | (true,  'g') -> GSharp
+            | (_, unknown) -> failwith <| string unknown 
+        )
+
+let parse_note = parse_nonSharpNote <|> parse_sharpNote
+let parse_octave = anyOf "123" |>> (function
+    | '1' -> One
+    | '2' -> Two
+    | '3' -> Three
+    | unknown -> failwith <| string unknown)
+
+let parse_tone  = pipe2 parse_note parse_octave (fun parsedNote parsedOctave -> Tone(parsedNote, parsedOctave))
+let parse_rest  = stringReturn "-" Rest
+let parse_token = pipe2 parse_length (parse_rest <|> parse_tone) (fun length tone -> { length = length; sound = tone })
+
+let parse_allTokens         = sepBy parse_token (pstring " ")
+let parse_composition input = run parse_allTokens input
