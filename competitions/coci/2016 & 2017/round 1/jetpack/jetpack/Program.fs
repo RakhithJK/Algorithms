fc2a7af0a86a4a26aec07e371b138154548d437b
Gabrijel Boduljak <gabrijel@dump.hr>
2016-10-31 21:47:41 +0100

    minor improvements to jetpack, started coursera algorithms part1 course, implemented quick union find

--- competitions/coci/2016 & 2017/round 1/jetpack/jetpack/Program.fs
+++ competitions/coci/2016 & 2017/round 1/jetpack/jetpack/Program.fs
@@ -77,18 +77,17 @@ let findStoppingPositionAbove searchRowIndex currentPosition =
     let row                     = getRowFromIndex searchRowIndex
     let doesRowContainObstacles = row.Any(fun x -> x = 'X')
 
-    let findAscendingPosition   = 
+    let findAscendingPosition()   = 
         let stopping = 
             row
             |> List.filter (fun el -> el = '.')
-            |> List.mapi   (fun columnIx el -> { row = searchRowIndex; column = columnIx }) 
-            |> List.rev 
-            |> List.find   (fun el -> el.row = searchRowIndex && el.column = currentPosition.column + 1)
+            |> List.mapi   (fun columnIx el -> { row = currentPosition.row - 1; column = columnIx })
+            |> List.find   (fun el -> el.row = currentPosition.row - 1 && el.column = currentPosition.column + 1)
         stopping
 
     let endPosition = 
         match doesRowContainObstacles with 
-        | true  -> findAscendingPosition
+        | true  -> findAscendingPosition()
         | false -> { row = currentPosition.row - 1; column = currentPosition.column + 1 }
 
     let substep = { startPosition = currentPosition; endPosition = endPosition; duration = Math.Abs (endPosition.row - currentPosition.row) }
@@ -98,7 +97,7 @@ let findPositionBelow searchRowIndex currentPosition =
     let row                     = getRowFromIndex searchRowIndex
     let doesRowContainObstacles = row.Any(fun x -> x = 'X')
 
-    let findDescendingPosition  = 
+    let findDescendingPosition()  = 
         let stopping = 
             row
             |> List.filter (fun el          -> el = '.')
@@ -108,7 +107,7 @@ let findPositionBelow searchRowIndex currentPosition =
 
     let endPosition =
         match doesRowContainObstacles with 
-        | true  -> findDescendingPosition
+        | true  -> findDescendingPosition()
         | false -> { row = currentPosition.row + 1; column = currentPosition.column + 1 }
 
     let substep = { startPosition = currentPosition; endPosition = endPosition; duration = Math.Abs (endPosition.row - currentPosition.row) }
@@ -125,9 +124,47 @@ let goRightOrDescend searchRowIndex currentPosition maxColumnIndex =
     let substep = findPositionBelow searchRowIndex currentPosition
     substep, (canDescend substep.endPosition maxColumnIndex)
 
+let goRight currentPosition currentSearchRow maxColumnIndex =
+    let row               = getRowFromIndex currentSearchRow
+    let doesRowContainObstacles = row.Any(fun x -> x = 'X')
+    let unexploredRowPart = row.[currentPosition.column .. maxColumnIndex]
+    let canMoves          = (loopUntil (fun el -> el = 'X') unexploredRowPart)
+
+    let takeElementIfPositionAfterCurrentPosition ix el = 
+                match ix with 
+                | ix when ix > currentPosition.column -> Some el
+                | _                                   -> None
+
+    let horizontalMoves  =
+        (fst canMoves) 
+            |> List.mapi    (takeElementIfPositionAfterCurrentPosition) 
+            |> List.filter  (fun option -> option.IsSome)
+            |> List.map     (fun el -> el.Value)
+
+    let findAscendingPosition = 
+        let stopping = 
+            horizontalMoves
+            |> List.filter (fun el -> el = '.')
+            |> List.mapi   (fun columnIx el -> { row = currentSearchRow; column = columnIx }) 
+            |> List.sortByDescending (fun el -> el.column) 
+            |> List.take 1
+            |> List.exactlyOne
+
+        stopping
+
+    let endPosition = 
+        match doesRowContainObstacles with 
+        | true  -> findAscendingPosition
+        | false -> { row = currentPosition.row - 1; column = currentPosition.column + 1 }
+
+    let substep = { startPosition = currentPosition; endPosition = endPosition; duration = Math.Abs (endPosition.row - currentPosition.row) }
+    substep, (canAscend substep.endPosition maxColumnIndex)    
+
+  
+    
 let solve map columnsCount = 
     let mutable stepsCount        = 0;
-    let mutable remainingColumns  = columnsCount - 1;
+    let mutable maxColumnIndex    = columnsCount - 1;
     let mutable currentPosition   = { row = 9; column = 0 }
     let mutable currentSearchRow  = 9
     let mutable currentTimestamp  = 0
@@ -137,14 +174,21 @@ let solve map columnsCount =
     let operations = new Queue<substep>()
     let steps      = new Queue<step>()
 
-    while remainingColumns <> 0 do
+    while currentPosition.column <> maxColumnIndex do
         let mutable shouldAscend  = canAscend  currentPosition maxColumnIndex
         let mutable shouldDescend = canDescend currentPosition maxColumnIndex
 
-        while shouldAscend = true && shouldDescend = false && remainingColumns <> 0 do
+        if (not shouldAscend && not shouldDescend || stepsCount = 0 && currentPosition.column <> maxColumnIndex) then
+            let horizontalMoves   = (goRight currentPosition currentSearchRow maxColumnIndex)
+            currentTimestamp <- 
+                match ((fst horizontalMoves).duration) with
+                | 0 -> currentTimestamp + 1 // horizontalMove
+                | _ -> currentTimestamp + ((fst horizontalMoves).duration)
+            currentPosition       <- (fst horizontalMoves).endPosition
+
+        while shouldAscend = true && shouldDescend = false && currentPosition.column <> maxColumnIndex do
             let performAscendOrGoRight = goRightOrAscend currentSearchRow currentPosition maxColumnIndex
             operations.Enqueue (fst performAscendOrGoRight)
-            remainingColumns <- remainingColumns - 1
             currentPosition  <- ((fst performAscendOrGoRight).endPosition)
             shouldAscend     <- (snd performAscendOrGoRight)
             shouldDescend    <- (not shouldAscend)
@@ -168,10 +212,14 @@ let solve map columnsCount =
 
         previousTimestamp <- currentTimestamp
 
-        while shouldAscend = false && shouldDescend = true && remainingColumns <> 0 do
+        currentSearchRow <- 
+            match shouldAscend with
+            | true  -> currentPosition.row - 1
+            | false -> currentPosition.row + 1
+
+        while shouldAscend = false && shouldDescend = true && currentPosition.column <> maxColumnIndex do
             let performDescendOrGoRight = goRightOrDescend currentSearchRow currentPosition maxColumnIndex
             operations.Enqueue (fst performDescendOrGoRight)
-            remainingColumns <- remainingColumns - 1
             currentPosition  <- ((fst performDescendOrGoRight).endPosition)
             shouldDescend    <- (snd performDescendOrGoRight)
             shouldAscend     <- (not shouldDescend)
@@ -186,29 +234,13 @@ let solve map columnsCount =
                 | 0 -> currentTimestamp + 1 // horizontalMove
                 | _ -> currentTimestamp + ((fst performDescendOrGoRight).duration)
 
-        if (not shouldAscend && not shouldDescend) then
-            let takeElementIfPositionAfterCurrentPosition ix el = 
-                match ix with 
-                | ix when ix > currentPosition.column -> Some el
-                | _                                   -> None
-                
-            let row             = getRowFromIndex currentSearchRow
-            let canMoves        = (loopUntil (fun el -> el = 'X') row)
-            let x               = (fst canMoves) @ (snd canMoves) // fix here for corner case
-            let horizontalMoves =
-                (fst (loopUntil (fun el -> el = 'X') row)) 
-                |> List.mapi    (takeElementIfPositionAfterCurrentPosition) 
-                |> List.filter  (fun option -> option.IsSome)
-                |> List.map     (fun el -> el.Value)
-
-            currentTimestamp    <- currentTimestamp + horizontalMoves.Length
-            currentPosition     <- { row = currentSearchRow; column = currentPosition.column + (horizontalMoves.Length - 1)}
-
         previousTimestamp <- currentTimestamp
 
-      
-            
-            
+        currentSearchRow <- 
+            match shouldAscend with
+            | true  -> currentPosition.row - 1
+            | false -> currentPosition.row + 1
+
     List.ofArray (steps.ToArray())
 
 [<EntryPoint>]
---
+++ courses/princeton-algorithms/union-find/quick-find.fsx
@@ -0,0 +1,36 @@
+type graph(verticesCount) = 
+    let mutable connectionsHolder  = Array.init verticesCount (fun index -> index) 
+    let mutable modificationsCount = 0 
+    member this.elementsCount     = verticesCount
+    member this.areConnected p q  = connectionsHolder.[p] = connectionsHolder.[q]
+    member this.connect      p q  = 
+        let performConnecting = 
+            let pValue = connectionsHolder.[p]
+            let qValue = connectionsHolder.[q]
+
+            connectionsHolder <-  
+                connectionsHolder |> 
+                Array.map(
+                    fun elementValue ->
+                        match elementValue with 
+                        | value when value = pValue -> qValue
+                        | _ -> elementValue
+                )
+            modificationsCount <- modificationsCount + 1
+            connectionsHolder
+
+
+        let arePandQConnected = this.areConnected p q
+        match arePandQConnected with
+        | true  -> connectionsHolder
+        | false -> performConnecting 
+        
+let graph  = graph 10
+let state1 = graph.connect 4 3
+let state2 = graph.connect 3 8
+let state3 = graph.connect 6 5
+let state4 = graph.connect 9 4
+let state5 = graph.connect 2 1
+
+let are8and9Connected = graph.areConnected 8 9
+let are5and0Connected = graph.areConnected 5 0
