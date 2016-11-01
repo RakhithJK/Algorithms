4460f8011db82cf703631925d2b4c2095f0a8c8b
Gabrijel Boduljak <gabrijel@dump.hr>
2016-11-01 13:57:56 +0100

    implemented weighted quick union finds with path compression

---
+++ courses/princeton-algorithms/union-find/weighted-quick-union-pathCompressed.fsx
@@ -0,0 +1,50 @@
+type graph(verticesCount) = 
+    let mutable connectionsHolder          = Array.init verticesCount (fun index -> index)   
+    let mutable subGraphSizesHolder        = Array.init verticesCount (fun index -> index)   
+    let mutable modificationsCount         = 0 
+    member this.elementsCount              = verticesCount
+    member this.findVertexRoot vertexIndex =
+        let rec searchForRootRecursively maybeRootIndex =
+            let value = connectionsHolder.[maybeRootIndex]
+            match maybeRootIndex with 
+            | index when index = value -> value
+            | _ -> searchForRootRecursively connectionsHolder.[connectionsHolder.[maybeRootIndex]] // flatten tree, nesting reduced
+        searchForRootRecursively vertexIndex
+
+    member this.areConnected p q = this.findVertexRoot p = this.findVertexRoot q
+    member this.connect      p q = 
+        let performConnection =
+            let pSubGraphSize = subGraphSizesHolder.[p]
+            let qSubGraphSize = subGraphSizesHolder.[q]
+
+            let pRoot = this.findVertexRoot p
+            let qRoot = this.findVertexRoot q
+
+            let putPSubgraphUnderQSubgraph() = 
+                connectionsHolder.[pRoot]   <- qRoot
+                subGraphSizesHolder.[pRoot] <- subGraphSizesHolder.[pRoot] + subGraphSizesHolder.[qRoot]
+
+            let putQSubgraphUnderPSubgraph() = 
+                connectionsHolder.[qRoot]   <- pRoot
+                subGraphSizesHolder.[qRoot] <- subGraphSizesHolder.[qRoot] + subGraphSizesHolder.[pRoot]
+
+            match (pSubGraphSize, qSubGraphSize) with
+            | sizes when qSubGraphSize >= pSubGraphSize -> putPSubgraphUnderQSubgraph()
+            | _                                         -> putQSubgraphUnderPSubgraph()
+        
+            modificationsCount <- modificationsCount + 1
+            connectionsHolder
+
+        match this.areConnected p q with
+        | true  -> connectionsHolder
+        | false -> performConnection 
+            
+let graph  = graph 10
+let state1 = graph.connect 4 3
+let state2 = graph.connect 3 8
+let state3 = graph.connect 6 5
+let state4 = graph.connect 9 4
+let state5 = graph.connect 2 1
+
+let are8and9Connected = graph.areConnected 8 9
+let are5and0Connected = graph.areConnected 5 0
