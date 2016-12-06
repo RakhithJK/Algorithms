3d5c04a2d5be0a9ec0d838a7e2795f0a75f82bfb
Gabrijel Boduljak <gabrijel@dump.hr>
2016-12-06 17:34:26 +0100

    solved advent of code day2.2 and day3.1

---
+++ competitions/adventOfCode2016/day2.2.fsx
@@ -0,0 +1,87 @@
+﻿let input = "LLLUDRDLLULDUDLLRLUDURULDURRRRLLURLDLDDDULLDDUDLRDLRDUURRDUUDLLRUUDDLULURDLRDUUDUDRURULLLLDRULDDLRDDRDLRDDLURLDDUDLLUUDLRDDDDLULUUURRDLUUDDLULLURRRDULLUDUDRDRDDRLDLLRLRDDDRDLULLUULDLLLRRDDUURUURDLLDRRDDLRULDLLDRLLUDRRDLUUULDLURLLDDURRUULLLLLRLUDLDDLLLURRRDUDULRULULDDLLDLRDDDUULRLRDUURLURRUDDURRUUDUDLDDLDULDDDDDULRULRDLRLDLRDLDDLUDRDUUDLDUDUDLLLRLUUDRUDDDRDRURRLLLDLUULDUULRDLULLUURLDRRRLDRLUDRRURLDULULDRUDDULLLDRDLLULUDDDDRDRULDULRRRRDDRULDLRDU
+DLDDRRDLLDUURRLULLLLRDRLUDURLDRRDURRRRUUDDUDRRDDDRRLDDLDDLURDLDRLUDULDUDUUDDDLLULRRLLUDULLLUULDDRDDUDUUDULURULULLDRLRUURDRDDLRRUDRUULLLLURRUDRDULDDRURRURLLLLLRLLLRLLUDUDLRDLULDUDLULLLUUDLLDDDUDUDLLRRDLRDLLLRRLRUDRDUDRURLUUURULLDDDDLLURDULURRLLLRLRRULLRRRLUUULLLLRLRDUURRDUURLLLDRDURRRULDDUDRRDLRLDLLRUDDLLUDRLLUUDRLLLLLLUDLURLLRUDRUUUULDLUDULLDDDDRLURUURDDDURRRLRLUDUUDURDDDDRRLRDLURDDLLULDRDDURLLURULUUUUURDUUULDRRLLURRRRRLDDUULLRULLDLDLDLRRRDRLDRUUD
+RLDRRRURULDLUDLDUDLLDUUURRDUDDURULLRRDDULUUDRRRULRUURRRLUUULRDRUDRRLLRLURDLDRDRDLLUDRUULRUDRUDDRURLRLURRDDRRURUUDRRDDRURURUDUUUDUDRRLRDRUUURLLUUUDLRUUDDRDDDDLDRLRDUDDULDDLRLLRURLLURLDDLDLDDULLDDUUURDLRUDUUDLDURDDRUULDRDDRDDDUUUDRDRDDRRDRRDLRDRURDUDDLUUUDULLUULULULRDRUUDDURURDRRRRLUDLDUDURLDRURDLLUUUDLRRDRRURDDULULURLDUDDLUDLDDLLRLDULLULULURUURLDULUDLLUUDLDDULDRRDDUULLUDLDLLRDRDURDDURDDURLDDURUURLLRURURUDDURRDRLRLDDUUDUULRDLLURRRRULURULDUDUDDUDDRLLLDLURDUURUURLUULRRLDLULDDRLDDUURULURUDRD
+URLDDRLLRRLDRLLRRURURURDDLRRRUUUURULRRUUDLUDRULLDLRUDDLULRUULDULURLLRLLUDDUDLURDRRRRLURULRURRURRULRRRULDLLDDLRLUDULUUUDDUDDRRDDDDUULRRLDRRULULRDUURRLDDRDULDURUDUDDLDLLURDDLDDRUDUUUDUUURDLDUDUUULLDLRDULRRRDLLURLDLLULRDDULULURLRLUULRLLLDDDUDLLDLURRRULRDUDDLULUDRUDDURULRLRUDDURLLURULLURDRULDUDLDULRRDLDURLUURRDDUDDUDRURUDDURRUUDURUULLLLDDRDDDDDULUUDDURRULLDRRLRRRRRDDRUUDDDURDRDRUDDUULDUDRRDRULUURLURLUDUDULDDRDULDLRUUDLLLRRLRDRDDUUULRDUDLUDLURRDUURDULDRLLDRDULDUDUULRLLDLRLDLUUDLRUULDUUULDLRLRLUULLLLRLRDUDRUUDURLDUDRRURLRUDRRLRDDLRDDLDDUDDDRLRLLRLUUURLURRRLULRLLDRLRDDRRDRL
+DLLLLLLRLRDRUDLRLLRLDLRURRUURLDLDDDDDUDUULLLLRRLRRDUUDUDLULLRRDULUDLLULURLRULURUULRLURDUDLUDULULUUURLRUDULURULRURULURLRLDRRRRLUDLLDULLDDLLULUURRULRDURDUUDDDURRUDLLLLRLDLUDDULLDUDDURURURRRRULDULULUDDUUDRLRLLLDLLLUUUURUDUUDLDLLRLRDDUULLUURLDDLRRDRLULDLULRULDLDURLULUURRRUDLLRDLUDDULRULULUDDURDLUUURDUUURDUDURLUUDRLUDRULUDDRRDLUUDLLLRDDDDDDLDURDDLDRDLUUDRULLUDRDLDULLULDDRUUDRRLRURRUULLRLRDUUURRDRRDULDDULUUDDURLULRLRURLLRRR"
+
+let testInput = "ULL
+RRDDD
+LURDL
+UUUUD"
+
+open System
+
+type Direction = Up | Down | Left | Right 
+type SingleNumberInstructions = { Directions : Direction list }
+type MultipleNumberInstructions = { Instructions : SingleNumberInstructions list }
+type MultipleNumberInstructionsAccumulator = { CurrentPosition : int * int; CalculatedNumbers : char list } 
+
+let parseDirection directionAsChar = 
+    match directionAsChar with 
+    | 'U' -> Up
+    | 'D' -> Down
+    | 'L' -> Left
+    | 'R' -> Right
+    | _ -> failwith "unknown direction"
+
+let parseSingleNumberInstructions (singleNumberInstructionsAsString : string) = { Directions = singleNumberInstructionsAsString.ToCharArray() |> Array.map parseDirection |> List.ofArray }
+let parseMultipleNumberInstructions (input : string) = { Instructions = input.Split('\n', '\r') |> Array.map parseSingleNumberInstructions |> List.ofArray }
+
+let mutable currentNumber = 0
+
+let seedFunc i j = 
+    currentNumber <- currentNumber + 1
+    currentNumber
+
+let keypad = [|
+    [|'X'; 'X'; '1'; 'X'; 'X'|]; 
+    [|'X'; '2'; '3'; '4'; 'X'|]; 
+    [|'5'; '6'; '7'; '8'; '9'|]; 
+    [|'X'; 'A'; 'B'; 'C'; 'X'|]; 
+    [|'X'; 'X'; 'D'; 'X'; 'X'|]; 
+|]
+let startKeyPosition = 2,0
+
+let isInKeypad x y = x >= 0 && y >= 0 && x <= 4 && y <= 4
+let canMove x y =
+    let isInKeypad = isInKeypad y x
+
+    match isInKeypad with 
+    | true -> 
+         let nextChar = keypad.[x].[y]
+         let isDigit = keypad.[x].[y] <> 'X'
+         isDigit
+    | false -> false
+    
+    
+let moveTo direction currentPosition =
+    let nextPosition = 
+        match direction with 
+        | Up   ->  fst currentPosition - 1, snd currentPosition 
+        | Down -> fst currentPosition + 1, snd currentPosition 
+        | Right -> fst currentPosition, snd currentPosition + 1
+        | Left -> fst currentPosition, snd currentPosition - 1
+
+    match (canMove (fst nextPosition) (snd nextPosition)) with 
+    | true  -> nextPosition
+    | false -> currentPosition
+
+let computeSingleNumber singleNumberInstructions currentPosition  =
+    let numberPosition = singleNumberInstructions.Directions |> List.fold (fun previousPosition instruction -> moveTo instruction previousPosition) currentPosition
+    numberPosition, keypad.[fst numberPosition].[snd numberPosition]
+
+let computeBathroomCode (multipleNumberInstructions : MultipleNumberInstructions) =
+    let sd accumulator instructions =
+        let computedNumber = computeSingleNumber instructions accumulator.CurrentPosition
+        { CurrentPosition = fst computedNumber; CalculatedNumbers = (snd computedNumber) :: accumulator.CalculatedNumbers }
+
+    multipleNumberInstructions.Instructions |> List.filter (fun instructions -> instructions.Directions.Length > 0) |> List.fold sd { CurrentPosition = startKeyPosition; CalculatedNumbers = []}
+
+let solve (input : string) =
+    let formNumberFromDigits computationResult = 
+        let digits = List.rev computationResult.CalculatedNumbers 
+        digits |> List.fold (fun number digit -> number + string digit) ""
+        
+    parseMultipleNumberInstructions input |> computeBathroomCode |> formNumberFromDigits
+
+solve input
---
+++ competitions/adventOfCode2016/day3.1.fsx
@@ -0,0 +1,36 @@
+﻿open System
+
+let parseLength lengthString = 
+    Int32.Parse lengthString
+let parseTriangle (triangleSidesString: string) = 
+    triangleSidesString.Split(' ')               |> 
+    Array.filter (fun sides -> sides.Length > 0) |> 
+    Array.map parseLength                        |>
+    List.ofArray
+
+let parseTriangles (inputString : string) = 
+    inputString.Split('\n', '\r') |> 
+    Array.map parseTriangle       |> 
+    List.ofArray
+
+let isValidTriangle (sides : int list) =
+    let longestSide = List.max sides
+    let longestSideIndex = sides |> List.findIndex (fun side -> side = longestSide)
+    let otherSides = 
+        sides                                              |> 
+        List.mapi (
+            fun index side ->
+                match index <> longestSideIndex with
+                | true -> Some side
+                | false -> None
+        )                                                  |>
+        List.filter (fun maybeOther ->  maybeOther.IsSome) |>
+        List.map (fun option -> option.Value)
+
+    List.sum otherSides > longestSide
+
+let countValidTriangles trianglesList = 
+    trianglesList 
+        |> List.filter (fun (sides : int list) -> sides.Length = 3) 
+        |> List.filter isValidTriangle 
+        |> List.length
