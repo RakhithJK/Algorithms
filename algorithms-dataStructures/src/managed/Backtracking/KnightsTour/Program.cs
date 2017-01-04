1f369e0b3a549fea608eb82c4493f1fe44d6be8d
Gabrijel Boduljak <gabrijel@dump.hr>
2017-01-04 02:03:24 +0100

    solved knight's tour problem with backtracking

---
+++ algorithms-dataStructures/src/managed/Backtracking/KnightsTour/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace KnightsTour
+{
+    class Program
+    {
+        static void Display(int[,] path)
+        {
+            for (int i = 0; i <= path.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= path.GetUpperBound(1); j++)
+                {
+                    if(path[i, j] / 10 == 0)
+                        Console.Write("0");
+
+                    Console.Write(path[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter chessboard length");
+            var chessboardLength = int.Parse(Console.ReadLine());
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var solverResult = new Solver().Solve(chessboardLength);
+            if (solverResult.CanBeSolved)
+                Display(solverResult.Path);
+            else
+                Console.WriteLine("cannot be solved!");
+
+            stopwatch.Stop();
+            Console.WriteLine($"solution took {stopwatch.Elapsed}.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ algorithms-dataStructures/src/managed/Backtracking/KnightsTour/Solver.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KnightsTour
+{
+    public class Solver
+    {
+        private int[,] path;
+        private int    pathLength;
+        
+        public class Result
+        {
+            public Result(bool canBeSolved, int[,] path)
+            {
+                CanBeSolved = canBeSolved;
+                Path        = path;
+            }
+
+            public bool   CanBeSolved { get; private set; }
+            public int[,] Path        { get; private set; }
+        }
+
+        private bool IsInMatrix(int row, int col, int n)
+            => row >= 0 && col >= 0 && row < n && col < n;
+
+        public bool Solve(int row, int column, int n)
+        {
+            pathLength++;
+            path[row, column] = pathLength;
+
+            if (pathLength == n*n)
+                return true;
+
+            var possibleDirections = new List<int[]>();
+            var rowOffsets         = new int[] { 2, 1, -1, -2, -2, -1, 1, 2 };
+            var colOffsets         = new int[] { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                var possibleDirection = new int[] { row + rowOffsets[i], column + colOffsets[i] };
+                if(IsInMatrix(possibleDirection[0], possibleDirection[1], n) && path[possibleDirection[0], possibleDirection[1]] == 0)
+                    possibleDirections.Add(possibleDirection);
+            }
+
+            if (possibleDirections.Any(direction => Solve(direction[0], direction[1], n)))
+            {
+                return true;
+            }
+
+            path[row, column] = 0;
+            pathLength--;
+            return false;
+        }
+
+        public Result Solve(int n)
+        {
+            path = new int[n,n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    path[i, j] = 0;
+                }
+            }
+
+            var canBeSolved = Solve(0, 0, n);
+            return new Result(canBeSolved, path);
+        }
+    }
+}
