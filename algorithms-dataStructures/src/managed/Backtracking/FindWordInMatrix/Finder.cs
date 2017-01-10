6a64519381b05ffa7d7a939c79dc5deb81db0c87
Gabrijel Boduljak <gabrijel@dump.hr>
2017-01-10 16:40:51 +0100

    solved find word in matrix with backtracking

---
+++ algorithms-dataStructures/src/managed/Backtracking/FindWordInMatrix/Finder.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindWordInMatrix
+{
+    public class Finder
+    {
+        public Finder(char[,] letters, string word)
+        {
+            this.letters    = letters;
+            this.wordToFind = word;
+        }
+
+        public class Result
+        {
+            public Result(bool isFound, int[,] path)
+            {
+                IsFound = isFound;
+                Path        = path;
+            }
+
+            public bool   IsFound     { get; private set; }
+            public int[,] Path        { get; private set; }
+        }
+
+        private readonly char[,] letters;
+        private readonly string  wordToFind;
+
+        private int[,] solutionMatrix;
+        private int    wordLength;
+
+        private bool IsInMatrix(int row, int col) 
+            => row >= 0 && col >= 0 && row <= letters.GetUpperBound(0) && col <= letters.GetUpperBound(1);
+
+        public bool FindRestRecursively(int currentRow, int currentColumn)
+        {            
+            if (wordLength >= wordToFind.Length)
+                return true;
+            if (wordToFind[wordLength] != letters[currentRow, currentColumn])
+                return false;
+
+            wordLength++;
+            solutionMatrix[currentRow, currentColumn] = wordLength;
+
+            var rowOffsets    = new[] { 0, 0,  1, -1, 1, 1, -1, -1 };
+            var columnOffsets = new[] {1, -1, 0, 0, 1, -1, 1, -1};
+
+            for (int i = 0; i < 8; i++)
+            {
+                var direction = new Tuple<int, int>(currentRow + rowOffsets[i], currentColumn + columnOffsets[i]);
+                if (
+
+                    IsInMatrix(direction.Item1, direction.Item2)          && // in matrix
+                    solutionMatrix[direction.Item1, direction.Item2] == 0 && // not visited
+                    FindRestRecursively(direction.Item1, direction.Item2)    // letters after found
+                )
+                {
+                    return true;
+                }
+            }
+
+            wordLength--;
+            solutionMatrix[currentRow, currentColumn] = 0;
+            return false;
+        }
+
+        public Result TryFind()
+        {
+            wordLength     = 0;
+            solutionMatrix = new int[letters.GetUpperBound(0) + 1, letters.GetUpperBound(1) + 1];
+            for (int i = 0; i < letters.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < letters.GetUpperBound(1); j++)
+                {
+                    if (letters[i, j] == wordToFind[0])
+                    {
+                        var isFound = FindRestRecursively(i, j);
+                        if (isFound)
+                            return new Result(true, solutionMatrix);
+                    }
+                }
+            }
+            return new Result(false, null);
+        }
+    }               
+}
---
+++ algorithms-dataStructures/src/managed/Backtracking/FindWordInMatrix/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWordInMatrix
+{
+    class Program
+    {
+        static void Display(int[,] path)
+        {
+            for (int i = 0; i <= path.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= path.GetUpperBound(1); j++)
+                {
+                    if (path[i, j] / 10 == 0)
+                        Console.Write("0");
+
+                    Console.Write(path[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var letters = new [,]
+            {
+                { 't', 'z', 'x', 'c', 'd' },
+                { 'a', 'h', 'n', 'z', 'x' }, 
+                { 'h', 'w', 'o', 'i', 'o' },
+                { 'o', 'r', 'n', 'r', 'n' }, 
+                { 'a', 'b', 'r', 'i', 'n' }
+            };
+
+            var wordToFind = "horizon";
+
+            var finder = new Finder(letters, wordToFind);
+            var result = finder.TryFind();
+            if(result.IsFound)
+                Display(result.Path);
+            else
+                Console.WriteLine("not found");
+            Console.ReadKey();
+        }
+    }
+}
