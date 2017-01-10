439d47d364f2de24b0656ed048f741b8f4030dbb
Gabrijel Boduljak <gabrijel@dump.hr>
2017-01-10 20:19:36 +0100

    solved maze problem with backtracking

---
+++ algorithms-dataStructures/src/managed/Backtracking/RatInAMazeProblem/Finder.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace RatInAMazeProblem
+{
+    public class Finder
+    {
+        public Finder(
+            bool[,]         maze, 
+            Tuple<int, int> startPosition,
+            Tuple<int, int> destinationPosition 
+        )
+        {
+            this.maze                = maze;
+            this.startPosition       = startPosition;
+            this.destinationPosition = destinationPosition;
+        }
+
+        public class Result
+        {
+            public Result(bool isFound, int[,] path)
+            {
+                IsFound = isFound;
+                Path    = path;
+            }
+
+            public bool     IsFound { get; private set; }
+            public int[,]   Path { get; private set; }
+        }
+
+        private readonly bool[,]            maze;
+        private readonly Tuple<int, int>    startPosition;
+        private readonly Tuple<int, int>    destinationPosition;
+        private          int[,]             solutionMatrix;
+        private          int                pathLength;
+        private bool IsInMatrix(int row, int col)
+            => row >= 0 && col >= 0 && row <= maze.GetUpperBound(0) && col <= maze.GetUpperBound(1);
+
+        private bool CanReachRestOfThePath(int row, int column)
+        {
+            if (destinationPosition.Item1 == row && destinationPosition.Item2 == column)
+            {
+                pathLength++;
+                solutionMatrix[row, column] = pathLength;
+                return true;
+            }
+
+            if (maze[row, column] == false)
+                return false;
+
+            pathLength++;
+            solutionMatrix[row, column] = pathLength;
+
+            var rowOffsets    = new[] { 0, 0, 1, -1 };
+            var columnOffsets = new[] { 1, -1, 0, 0 };
+
+            for (int i = 0; i < 4; i++)
+            {
+                var position = new Tuple<int, int>(row + rowOffsets[i], column + columnOffsets[i]);
+                if (
+                    IsInMatrix              (position.Item1, position.Item2)          &&
+                    solutionMatrix          [position.Item1, position.Item2] == 0     &&
+                    CanReachRestOfThePath   (position.Item1, position.Item2)   
+                )
+                {
+                    return true;
+                }
+            }
+
+            pathLength--;
+            solutionMatrix[row, column] = 0;
+            return false;
+        }
+
+        public Result TryFind()
+        {
+            solutionMatrix = new int[maze.GetUpperBound(0) + 1, maze.GetUpperBound(1) + 1];
+            pathLength     = 0;
+            var canReachDestination = CanReachRestOfThePath(startPosition.Item1, startPosition.Item2);
+            return canReachDestination
+                ? new Result(true, solutionMatrix)
+                : new Result(false, null); 
+        }
+    }
+}
---
+++ algorithms-dataStructures/src/managed/Backtracking/RatInAMazeProblem/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatInAMazeProblem
+{
+    class Program
+    {
+        static void Display(int[,] path)
+        {
+            for (int i = 0; i <= path.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= path.GetUpperBound(1); j++)
+                {
+                    if (path[i, j] / 10 == 0)
+                        Console.Write("0");
+
+                    Console.Write(path[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var maze = new[,]
+            {
+                {true, false, true, true, true},
+                {true, true, true, false, true},
+                {false, false, false, true, true},
+                {false, false, false, true, false},
+                {false, false, false, true, true}
+            };
+            var startPosition       = new Tuple<int, int>(0,0);
+            var destinationPosition = new Tuple<int, int>(maze.GetUpperBound(0), maze.GetUpperBound(1));
+            var result              = new Finder(maze, startPosition, destinationPosition).TryFind();
+
+            if (result.IsFound)
+                Display(result.Path);
+            else
+                Console.WriteLine("not found");
+            Console.ReadKey();
+        }
+    }
+}
