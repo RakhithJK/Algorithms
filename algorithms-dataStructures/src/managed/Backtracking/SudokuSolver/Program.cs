4063781c4c26fcf335e8f18540025921838c4b84
Gabrijel Boduljak <gabrijel@dump.hr>
2017-01-11 20:36:51 +0100

    wrote sudoku solver with backtracking

---
+++ algorithms-dataStructures/src/managed/Backtracking/SudokuSolver/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        static void Display(int[,] path)
+        {
+            for (int i = 0; i <= path.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= path.GetUpperBound(1); j++)
+                {
+                    if (path[i, j] / 10 == 0)
+                        Console.Write(' ');
+
+                    Console.Write(path[i, j]);
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var gridToSolve = new [,]
+            {
+                { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+                { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+                { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+                { 8, 0, 0, 0, 6, 0, 0, 0, 3 }, 
+                { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+                { 7, 0, 0, 0, 2, 0, 0, 0, 6 }, 
+                { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+                { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+                { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
+            };
+               
+            Console.WriteLine("grid to solve :");
+            Display(gridToSolve);
+            Console.WriteLine();
+
+            var result = new Solver(gridToSolve).TrySolve();
+
+            if (result.CanBeSolved)
+            {
+                Console.WriteLine("solved grid :");
+                Display(result.SolvedGrid);
+            }
+            else
+                Console.WriteLine("cant be solved");
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ algorithms-dataStructures/src/managed/Backtracking/SudokuSolver/Solver.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+
+namespace SudokuSolver
+{
+    public class Solver
+    {
+        public Solver(int [,] gridToSolve)
+        {
+            this.gridToSolve = gridToSolve;
+        }
+        private readonly int[,] gridToSolve;
+
+        private bool CanPlaceNumber(int row, int col, int number)
+        {
+            var boxStartRow    = row - row % 3;
+            var boxStartColumn = col - col % 3;
+
+            var canPlaceInRow = 
+                Enumerable
+                    .Range(0, gridToSolve.GetUpperBound(1))
+                    .All(gridCol => gridToSolve[row, gridCol] != number);
+
+            var canPlaceInColumn =
+                Enumerable
+                    .Range(0, gridToSolve.GetUpperBound(0))
+                    .All(gridRow => gridToSolve[gridRow, col] != number);
+
+            var canPlaceInBox = true;
+
+            for (var i = 0; i < 3 && canPlaceInBox; i++)
+            {
+                for (var j = 0; j < 3 && canPlaceInBox; j++)
+                {
+                    if (gridToSolve[boxStartRow + i, boxStartColumn + j] == number)
+                        canPlaceInBox = false;
+                }   
+            }
+
+            return canPlaceInRow && canPlaceInColumn && canPlaceInBox;
+        }
+
+        private Tuple<bool, int, int> FindNextUnsolvedPositionIfExists()
+        {
+            for (var i = 0; i <= gridToSolve.GetUpperBound(0); i++)
+            {
+                for (var j = 0; j <= gridToSolve.GetUpperBound(1); j++)
+                {
+                    if(gridToSolve[i, j] == 0)
+                        return new Tuple<bool, int, int>(true, i, j);
+                }
+            }
+            return new Tuple<bool, int, int>(false, int.MinValue, int.MinValue);
+        }
+
+        private bool CanFillNextUnsolvedPosition()
+        {
+            var nextUnsolvedPosition = FindNextUnsolvedPositionIfExists();
+            if (!nextUnsolvedPosition.Item1)
+                return true;
+
+            var possibleNumbers =
+                Enumerable
+                    .Range(1, 9)
+                    .Where(number => CanPlaceNumber(nextUnsolvedPosition.Item2, nextUnsolvedPosition.Item3, number));
+
+            foreach (var possibleNumber in possibleNumbers)
+            {
+                gridToSolve[nextUnsolvedPosition.Item2, nextUnsolvedPosition.Item3] = possibleNumber;
+
+                if (CanFillNextUnsolvedPosition())
+                    return true;
+
+                gridToSolve[nextUnsolvedPosition.Item2, nextUnsolvedPosition.Item3] = 0;
+            }
+
+            return false;
+        }
+
+        public Result TrySolve()
+        {
+            var canBeSolved = CanFillNextUnsolvedPosition();
+            return canBeSolved
+                ? new Result(true, gridToSolve)
+                : new Result(false, null);
+        }
+
+        public class Result
+        {
+            public Result(
+                bool    canBeSolved, 
+                int[,]  solvedGrid)
+            {
+                CanBeSolved = canBeSolved;
+                SolvedGrid  = solvedGrid;
+            }
+
+            public bool     CanBeSolved     { get; private set; }
+            public int[,]   SolvedGrid      { get; private set; }
+        }
+    }
+}
